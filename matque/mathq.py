814443fdb822f7608186b625176529779517c604
Brian Welman <brianallisterwelman@gmail.com>
2022-09-01 14:19:47 +0200

    Transferring contents

--- matque/mathq.py
+++ matque/mathq.py
@@ -2,50 +2,6 @@ import numpy as np
 import sympy as sp
 from abc import ABC, abstractmethod
 
-
-class Error(Exception):
-
-    def __init__(self, code, message, remedy):
-        self.code = code
-        self.message = message
-        self.remedy = remedy
-        super().__init__(message)
-
-
-    def __str__(self):
-        return f"""
-        Error-code:
-            {self.code}
-        Details:
-            {self.message}
-        Remedy:
-            {self.remedy}
-        """
-
-
-class InvalidOptionError(Error):
-
-    def __init__(self, obj, option):
-        self.obj = obj
-        self.option = option
-        self.code = "Invalid Option"
-        self.message = f"`{obj}` has no option `{option}`"
-        self.remedy = f"Avaliable options: {list(obj.options.keys())}"
-        super().__init__(self.code, self.message, self.remedy)
-
-
-class OptionValueError(Error):
-
-    def __init__(self, obj, option, value, correct_values):
-        self.obj = obj
-        self.option = option
-        self.value = value
-        self.code = "Invalid Option Value"
-        self.message = f"`{value}` for option `{option}` in `{obj}` is not valid."
-        self.remedy = f"Avaliable choices: {list(obj.options[option])}"
-        super().__init__(self.code, self.message, self.remedy)
-
-
 class MathqObject(object):
 
     def __init__(self):
