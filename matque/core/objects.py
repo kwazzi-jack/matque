acc598ead83d3214eeb831e4aef5cb223f9e19f3
Brian Welman <brianallisterwelman@gmail.com>
2022-09-01 20:02:08 +0200

    Created MatqueObject class for general object handling

--- matque/core/objects.py
+++ matque/core/objects.py
@@ -1,49 +1,46 @@
-from options import Options
+from omegaconf import OmegaConf, open_dict
+from omegaconf.dictconfig import DictConfig
 
 
-class MatqueOptions(Options):
-    """
-    Base options class for `matque` objects to utilize, with added support
-    to ensure correct option parsing.
-    """
-    pass 
-
-
-class MatqueObject(object):
+class MatqueObject:
     """
     Base object for all `matque` objects to inherit from to
     ensure option parsing and method handling.
 
     Attributes
-    ==========
-    options : MatqueOptions
-        contains options required by MatqueObject
-    name : str
-        identifier for `matque` object
+    ----------
+    options : omegaconf.dictconfig.DictConfig
+        contains read-only *class* options for `MatqueObject`, 
+        or *class* and *instance* options for instance of `MatqueObject`.
     """
     
-    # Default class options
-    options = MatqueOptions(
-                name = "MatqueObject"
-            )
+    # Default class options, read-only
+    options = OmegaConf.create()
+    OmegaConf.set_struct(options, True)
 
-    def __init__(self, **kwargs):
+
+    def __init__(self, **options):
         """
-        Create instance of `MatqueObject` with given options.
+        Create instance of `MatqueObject` with copy of class options
+        and update with new instance specific options. These options
+        are immutable.
 
         Parameters
         ----------
-        name
-
-        See also
-        --------
-        class `MatqueObject`, class `MatqueOptions`
+        options
         """
 
-        # Parse input options to options
-        self.options = MatqueObject.options.push(kwargs)
+        # Create new instance options
+        instance_options = OmegaConf.create(options)
+
+        # Merge class and instance options
+        with open_dict(MatqueObject.options):
+            self.options = OmegaConf.merge(
+                            MatqueObject.options, instance_options)
+
+        # Set options to read-only
+        OmegaConf.set_struct(self.options, True)
 
 
 if __name__ == "__main__":
-    options = MatqueOptions()
-    print(MatqueObject().options.name)
+    pass
