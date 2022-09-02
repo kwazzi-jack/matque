dd898682594e40ba2b0a7a145e3b11ac473603e5
Brian Welman <brianallisterwelman@gmail.com>
2022-09-02 11:19:32 +0200

    Fixed  var to instantiate with new subclasses

--- matque/core/objects.py
+++ matque/core/objects.py
@@ -6,37 +6,13 @@ class MatqueObject:
     """
     Base object for all `matque` objects to inherit from to
     ensure option parsing and method handling.
-
-    Attributes
-    ----------
-    options : omegaconf.dictconfig.DictConfig
-        contains read-only *class* options for `MatqueObject`, 
-        or *class* and *instance* options for instance of `MatqueObject`.
     """
     
-    # Default class options, read-only
-    options = OmegaConf.create()
-    OmegaConf.set_struct(options, True)
-
-
-    def __init__(self, **options):
-        """
-        Create instance of `MatqueObject` with copy of class options
-        and update with new instance specific options. These options
-        are immutable.
-
-        Parameters
-        ----------
-        options
-        """
-
-        # Create new instance options
-        instance_options = OmegaConf.create(options)
-
-        # Merge class and instance options
-        with open_dict(MatqueObject.options):
-            self.options = OmegaConf.merge(
-                            MatqueObject.options, instance_options)
-
-        # Set options to read-only
-        OmegaConf.set_struct(self.options, True)
+    def __init__(self) -> None:
+        
+        # Default class options
+        self.options = OmegaConf.create({
+            "name" : "MatqueObject"
+        })
+        
+        #OmegaConf.set_struct(self.options, True)
--- matque/expressions/__init__.py
+++ matque/expressions/__init__.py
@@ -1,5 +1,19 @@
 from matque.core.objects import MatqueObject
+from omegaconf import OmegaConf, open_dict
 
 
 class Expression(MatqueObject):
-    pass
+
+    def __init__(self, **options):
+        super().__init__()
+        
+        with open_dict(self.options):        
+            self.options.update(options)
+        
+
+
+if __name__ == "__main__":
+    x = Expression(name="Expressions", id=12345)
+    y = MatqueObject()
+    print(x.options)
+    print(y.options)
