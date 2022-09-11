a5779a52fd6e24817744431360e09554437ebc04
Brian Welman <brianallisterwelman@gmail.com>
2022-09-11 22:01:40 +0200

    Created init file and moved Macros to it

--- matque/core/objects.py
+++ matque/core/objects.py
@@ -4,31 +4,7 @@ from omegaconf.dictconfig import DictConfig
 from sympy.core.basic import Basic
 from sympy import sympify
 from typing import Union
-from enum import Enum
-
-
-# Macros
-class Macros(Enum):
-
-    # Errors (0 - 99)
-    ERROR = 0
-
-    # Types (100 - 200)
-    INTEGER = 100
-    DECIMAL = 101
-    VARIABLE = 102
-    RATIONAL = 103
-    IRRATIONAL = 104
-    FRACTION = 105
-    FUNCTION = 106
-    EQUALITY = 107
-    INEQUALITY = 108
-    GRAPH = 109
-    STRING = 110
-
-    # Constants (None)
-    MAX_INT = int(2**32 - 1)
-    MIN_INT = -MAX_INT
+from matque.core import Macros
 
 
 class MatqueObject:
