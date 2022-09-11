b069a2bb523ab9baf304694569fc754efc0d8f72
Brian Welman <brianallisterwelman@gmail.com>
2022-09-11 14:34:14 +0200

    Created generators for int and float

--- matque/generator/__init__.py
+++ matque/generator/__init__.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from sympy import Integer, Float
 
 
 def seed(n : int = None) -> None:
@@ -8,8 +9,9 @@ def seed(n : int = None) -> None:
     previously being used.
 
     Args:
-        n (int, optional): Integer to set as seed. 
-            Default generates its own seed.
+        n (int, optional): 
+            Integer to set as seed. Default generates 
+            its own seed.
     """
 
     # Choose random time of 32-bit system
@@ -21,13 +23,44 @@ def seed(n : int = None) -> None:
     np.random.seed(n)
 
 
-if __name__ == "__main__":
-    seed(100)
-    print("n:", 100)
-    print("Random:", random.random())
-    print("Numpy:", np.random.random())
+def integer(l: int, u: int) -> Integer:
+    """Randomly generate an integer in the given 
+    range of `[l, u]` and return Integer
 
-    seed()
-    print("n: auto")
-    print("Random:", random.random())
-    print("Numpy:", np.random.random())
+    Args:
+        l (int): 
+            Inclusive lowerbound of integer range.
+        u (int): 
+            Inclusive upperbound of integer range.
+
+    Returns:
+        Integer: 
+            Sympy version of integer randomly generated.
+    """
+
+    return Integer(random.randint(a=l, b=u))
+
+
+def decimal(l: int, u: int, r: int=2) -> Float:
+    """Randomly generate a float in the given
+    range of `[l, ]
+
+    Args:
+        l (int): _description_
+        u (int): _description_
+        r (int, optional): _description_. Defaults to 2.
+
+    Returns:
+        Float: _description_
+    """
+    num = random.uniform(l, u)
+    return Float(num, r + len(str(int(num))))
+
+
+if __name__ == "__main__":
+    l, u = 0, 100
+    x1 = integer(l, u)
+    print(x1)
+
+    x2 = decimal(l, u)
+    print(x2)
